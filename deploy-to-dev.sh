c6f3762fe1c4f9a250d2c1a8342cc1f4f6b9e507
Balasubramani K <bakuppus@gmail.com>
2019-03-04 04:14:35 +0530

    cc

---
+++ deploy-to-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export NODE_IP=$(kubectl describe svc dev-service | grep  addresses | cut -d '"' -f4)
+echo $NODE_IP
+curl -v -u admin:admin -T target/javaee7-simple-sample.war 'http://$NODE_IP:30000/manager/text/deploy?path=/dev&update=true'
